6e8a3cf2a123fbb9d1ee7d723c6ceb9714b13d63
Dingdingding110 <1834939993@qq.com>
2025-07-16 20:11:09 +0800

    活动部分4个接口（提交者：dc&lzw）

--- services/activity_service.py
+++ services/activity_service.py
@@ -20,3 +20,19 @@ def get_activity_by_id(activity_id: int):
 
     return {"message": f"Activity {activity_id} endpoint"}
 
+def get_header(activity_id: int):
+    """
+    获取用户的access_token
+    """
+    # 从数据库获取header数据
+
+    return{"Authorization": f"Bearer {"token"}"}
+
+def get_banner():
+    """
+    获取banner数据
+    """
+    # 从数据库获取banner数据
+
+    return{"message": "Banner endpoint"}
+
